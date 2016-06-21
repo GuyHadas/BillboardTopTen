20bf1036c29c4cc97a24609a809c25d62b490f88
Guy Hadas <GuyHadas195@gmail.com>
2016-06-21 15:07:02 -0700

    add billboard data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+charts = {}
+
+
+lines = File.readlines("billboard-data.txt")
+lines.each.with_index do |line, idx|
+  next unless idx % 63 == 1
+  date_key = lines[idx][0..-2]
+  charts[date_key] = []
+
+  i = 0
+  while i < 10
+    factor = 6 * i
+    track = {
+      title: lines[idx + 1 + (factor)][0..-2],
+      artist: lines[idx + 2 + (factor)][0..-2],
+      rank: lines[idx + 4 + (factor)][0..-2],
+      spotify_id: lines[idx + 5 + (factor)][0..-2],
+      spotify_link: lines[idx + 6 + (factor)][0..-2]
+    }
+    charts[date_key] << track
+    i += 1
+  end
+end
+
+p charts
