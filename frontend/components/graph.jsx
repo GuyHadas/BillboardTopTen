c748cc1877d55fb6a1d0881556fc72fc705a1add
Guy Hadas <GuyHadas195@gmail.com>
2016-10-25 23:21:28 -0700

    Refactor toDate

--- frontend/components/graph.jsx
+++ frontend/components/graph.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import _ from "lodash";
 
 import Track from './track.jsx';
 import { Title } from './title.jsx';
@@ -9,23 +10,24 @@ class Graph extends React.Component{
     super(props);
     this.toDate = this.toDate.bind(this);
   }
+
   toDate(date) {
     const months = {
-      "01": "January",
-      "02": "February",
-      "03": "March",
-      "04": "April",
-      "05": "May",
-      "06": "June",
-      "07": "July",
-      "08": "August",
-      "09": "September",
-      "10": "October",
-      "11": "November",
-      "12": "December"
+      '01': 'January',
+      '02': 'February',
+      '03': 'March',
+      '04': 'April',
+      '05': 'May',
+      '06': 'June',
+      '07': 'July',
+      '08': 'August',
+      "09": 'September',
+      '10': 'October',
+      '11': 'November',
+      '12': 'December'
     };
 
-    return months[date.slice(5, 7)] + " " + date.substring(8, 10) + ", " + date.substring(0, 4);
+    return `${months[date.slice(5, 7)]} ${date.substring(8, 10)}, ${date.substring(0, 4)}`;
   }
 
   render() {
@@ -54,9 +56,9 @@ class Graph extends React.Component{
     });
 
     return (
-      <div id="graph">
+      <div id='graph'>
         <Title date={this.toDate(this.props.date)} artist={this.props.chart[0].artist} />
-        <ul id="trackList">
+        <ul id='trackList'>
           {tracks}
           {tracksOnDeck}
         </ul>
