41c96f1cccc10caa3fed2870748266750643d986
Guy Hadas <GuyHadas195@gmail.com>
2016-11-22 00:37:57 -0800

    Add glow and navbar updates

--- frontend/components/title.jsx
+++ frontend/components/title.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 
 export const Title = ({artist, date}) =>(
     <div id='titleBox'>
+      <div id="navLogo">
+        <img id="billboard-logo" src="billboard-logo.png" width='100'/>
+        <span id="genre-title">Hot 100</span>
+      </div>
       <span id='titleArtist'>{artist}</span>
+      <span>was #1 on</span>
       <span id='titleDate'>{date}</span>
     </div>
 );
