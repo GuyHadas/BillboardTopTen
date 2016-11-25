a3f2f947b480dc61ce7ff8609c913a72bc3b521a
Guy Hadas <GuyHadas195@gmail.com>
2016-11-25 13:27:13 -0800

    Style tabs

--- frontend/components/tabs.jsx
+++ frontend/components/tabs.jsx
@@ -24,11 +24,37 @@ class Tabs extends React.Component{
       <Genres playGenre={this.props.playGenre}/>
     ][this.state.tabIndex];
 
+    let genreTabBackgroundColor, genreTabColor, datePickerTabBackgroundColor, datePickerTabColor;
+    if (this.state.tabIndex === 0) {
+      datePickerTabBackgroundColor = '#FF6961';
+      datePickerTabColor = 'black';
+    } else {
+      genreTabBackgroundColor = '#FF6961';
+      genreTabColor = 'black';
+    }
+
     return (
       <div id='tabs'>
         <div id='tabHeaders'>
-          <div className="tab" onClick={ () => this.switchTabs(0) } id='datePickerTab'>Datepicker</div>
-          <div className="tab" onClick={ () => this.switchTabs(1) } id='genreTab'>Genre</div>
+          <div className="tab"
+            onClick={ () => this.switchTabs(0) }
+            id='datePickerTab'
+            style={{
+              backgroundColor: datePickerTabBackgroundColor,
+              color: datePickerTabColor
+            }}>
+            Datepicker
+          </div>
+          <div
+            className="tab"
+            onClick={ () => this.switchTabs(1) }
+            id='genreTab'
+            style={{
+              backgroundColor: genreTabBackgroundColor,
+              color: genreTabColor
+            }}>
+            Genre
+          </div>
         </div>
         {renderedTab}
       </div>
