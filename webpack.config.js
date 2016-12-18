4b4cc86d67c6db3020898ada58ba1da3a52c109c
Avinoam Waizman <Avi.waizman@gmail.com>
2016-12-17 18:15:23 -0800

    Move images set production (#19)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var path = require("path");
+var webpack = require("webpack");
+
 module.exports = {
   context: __dirname,
   entry: "./frontend/billboard.jsx",
@@ -5,19 +8,32 @@ module.exports = {
     path: "./app/assets/javascripts",
     filename: "bundle.js"
   },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: false
+      }
+    })
+  ],
+
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
-        exclude: /node_modules/,
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['es2015', 'react', 'stage-2']
         }
       }
     ]
   },
-  devtool: 'source-map',
+  devtool: 'source-maps',
   resolve: {
     extensions: ["", ".js", ".jsx"]
   }
